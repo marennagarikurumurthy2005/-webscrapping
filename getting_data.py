d1a0216b4e66fa8b09ce2048bea012a6fd427abc
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-08 15:08:27 +0530

    soup and soup.prettify()

--- getting_data.py
+++ getting_data.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 
 
 response=requests.get("https://www.flipkart.com/")
+responsedata=response.content
 
 if response.status_code==200:
-    print("Success")
+    soup=BeautifulSoup(responsedata,"html.parser")
+    
+    
+    data=soup.prettify()
+    
     
 
 
